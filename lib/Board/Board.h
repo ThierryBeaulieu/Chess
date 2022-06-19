2e84c4c0a582269d900bf3b58b7e26f9c6ce74a9
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-06-19 15:57:24 -0400

    Converting the array to an array of pointers@

--- lib/Board/Board.h
+++ lib/Board/Board.h
@@ -5,16 +5,24 @@ template<int N, typename T>
 class Board
 {
 public:
+    Board(){
+        // initialise the array
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                array_[i][j] = nullptr;
+            }
+        }
+    }
     int getSize() {
         return N;
     }
     
-    int getElement(int x, int y){
+    T* getElement(int x, int y){
         return array_[x][y];
     }
     
 private:
-    T array_[N][N];
+    T* array_[N][N];
 };
 
 #endif // BOARD_H
