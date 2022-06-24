758c778d6b0706ff210ac61d92dccd193e63d3c6
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-06-23 22:19:24 -0400

    Sharing data from flask into React

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState({ members: ['bonjour', 'hello'] });
+
+  useEffect(() => {
+    fetch('http://localhost:5001/')
+      .then((response) => response.json())
+      .then((res) => {
+        setData(res);
+        console.log(res);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.members.map((member, i) => (
+        <p key={i}>{member}</p>
+      ))}
     </div>
   );
 }
--- server/ChessServer/app.py
+++ server/ChessServer/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
-    return "Basic html file here for Chess!"
-
+    return {"members": ["C++", "Java", "Python"]}
+ 
 # main
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
