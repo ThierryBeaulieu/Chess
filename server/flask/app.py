fec94cfd38c53bfd29e6c7d9c2cca5dc5fadf6ae
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-06-26 01:16:19 -0400

    Removing a TODO comment

--- server/flask/app.py
+++ server/flask/app.py
@@ -4,10 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def index():
     return {"members": ["C++", "Java", "Python"]}
- 
+
+
 # main
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
