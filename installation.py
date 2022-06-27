ced786a9fdec07a870ef25c2b8bf4886a93a8058
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-06-27 00:11:52 -0400

    Subprocess automate the installation of the front end

--- installation.py
+++ installation.py
@@ -5,13 +5,13 @@ import os
 # verify that the python version is at least 3
 pythonVersion = sys.version
 if pythonVersion[0] != '3':
-    
+
     print("Your python version should be at least 3.x.x")
     exit()
 
 # adds the dependcy of the submodules from git
-subprocess.run('git submodule init')
-subprocess.run('git sumbodule update')
+subprocess.run('git submodule init', shell=True)
+subprocess.run('git submodule update', shell=True)
 
 initialDir = os.getcwd()
 os.chdir(initialDir + '/server/flask/')
@@ -19,3 +19,7 @@ os.chdir(initialDir + '/server/flask/')
 # if pip3 doesn't work, you might want to change this to pip
 result = subprocess.run('pip3 install -r requirements.txt', shell=True)
 
+# installation the front end libraries
+print(initialDir)
+os.chdir(initialDir + '/client/')
+subprocess.check_call('npm install', shell=True)
