6419845f047975b5ffb21e50133bcb057f7f6f55
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-02 11:47:45 -0400

    Creating a more modular connection between React and the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,12 @@ import httpServer from './services/http.server';
 function App() {
   const [data, setData] = useState({ members: ['bonjour', 'hello'] });
 
+  async function fetchData() {
+    setData(await httpServer.GET(''));
+  }
+
   useEffect(() => {
-    async function fetchData() {
-      await httpServer.GET('');
-    }
-    const fetchedData = fetchData();
-    console.log(fetchedData);
-    setData(fetchedData);
+    fetchData();
   }, []);
 
   return (
--- client/src/services/http.server.jsx
+++ client/src/services/http.server.jsx
@@ -1,11 +1,9 @@
 const HTTP_SERVER = {
-  SERVER_LOCAL_PATH: 'http//localhost:5001',
+  SERVER_LOCAL_PATH: 'http://localhost:5001',
 
   async GET(endpoint) {
-    const response = await fetch(this.SERVER_LOCAL_PATH);
-    console.log(response);
-    console.log(this.SERVER_LOCAL_PATH);
-    return response.json();
+    const promise = await fetch(`${this.SERVER_LOCAL_PATH}/${endpoint}`);
+    return promise.json();
   },
 };
 
