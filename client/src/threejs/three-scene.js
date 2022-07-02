b3b1b3e8dd45903aa14053ee38daa8b0f36c4012
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-01 23:37:38 -0400

    adding a floor an a ball with directionnal light

--- client/src/threejs/three-scene.js
+++ client/src/threejs/three-scene.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 class ThreeScene extends Component {
   componentDidMount() {
@@ -10,6 +11,8 @@ class ThreeScene extends Component {
     //renderer
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.shadowMap.enable = true;
+    this.renderer.shadowMap.type = THREE.PCFShadowMap;
 
     this.mount.appendChild(this.renderer.domElement);
 
@@ -20,20 +23,58 @@ class ThreeScene extends Component {
       0.1,
       1000,
     );
-    this.camera.position.z = 5;
+    this.camera.position.z = 10;
 
+    //Create a DirectionalLight and turn on shadows for the light
+    this.light = new THREE.DirectionalLight(0xffffff, 1);
+    this.light.position.set(0, 1, 0); //default; light shining from top
+    this.light.castShadow = true; // default false
+    this.scene.add(this.light);
+
+    //Set up shadow properties for the light
+    this.light.shadow.mapSize.width = 512; // default
+    this.light.shadow.mapSize.height = 512; // default
+    this.light.shadow.camera.near = 0.5; // default
+    this.light.shadow.camera.far = 500; // default
+
+    /*
     // Create a Box
     var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
     var material = new THREE.MeshBasicMaterial({
       color: 0xc4c4c4,
     });
     this.cube = new THREE.Mesh(geometry, material);
-
-    // add the cube to the scene
+    this.cube.castShadow = true;
+    this.cube.receiveShadow = false;
     this.scene.add(this.cube);
-    this.animation();
-    //this.renderer.render(this.scene, this.camera);
+    */
 
+    //Create a sphere that cast shadows (but does not receive them)
+    this.sphereGeometry = new THREE.SphereGeometry(2, 32, 32);
+    this.sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    this.sphere = new THREE.Mesh(this.sphereGeometry, this.sphereMaterial);
+    this.sphere.castShadow = true; //default is false
+    this.sphere.receiveShadow = false; //default
+    this.scene.add(this.sphere);
+
+    // Create a plane that will receive shadow
+    this.planeGeometry = new THREE.PlaneGeometry(40, 30, 32, 32);
+    this.planeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    this.plane = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
+    this.plane.position.y -= 10;
+    this.plane.rotation.x = -Math.PI / 2;
+    this.plane.receiveShadow = true;
+    this.scene.add(this.plane);
+
+    //Create a helper for the shadow camera (optional)
+    this.helper = new THREE.CameraHelper(this.light.shadow.camera);
+    this.scene.add(this.helper);
+
+    // animate & render
+    this.animation();
+    this.renderer.render(this.scene, this.camera);
+
+    new OrbitControls(this.camera, this.renderer.domElement);
     // Event handler
     window.addEventListener('resize', this.handleWindowResize);
   }
@@ -47,8 +88,8 @@ class ThreeScene extends Component {
 
   animation = () => {
     requestAnimationFrame(this.animation);
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    //this.cube.rotation.x += 0.01;
+    //this.cube.rotation.y += 0.01;
     this.renderer.render(this.scene, this.camera);
   };
 
