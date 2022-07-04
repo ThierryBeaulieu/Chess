993e25ab5ff2d7369313561f15ebcc678886bac7
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-03 23:41:20 -0400

    removing disfunctionnal items

--- client/src/screens/MainPage.jsx
+++ client/src/screens/MainPage.jsx
@@ -5,7 +5,7 @@ export default function MainPage() {
   return (
     <>
       <div className='ColumnSeparator'>
-        <div>
+        <div className='ButtonColumn'>
           <div className='MainTitle'>Chess .</div>
           <Button name='Play online' />
           <Button name='Train yourself' secondary='true' />
