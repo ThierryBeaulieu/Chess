c5569691a9b692d0fba0022315da7089f27ee856
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-03 23:49:20 -0400

    deleting formating

--- client/src/screens/MainPage.jsx
+++ client/src/screens/MainPage.jsx
@@ -4,12 +4,10 @@ import Button from '../components/Button';
 export default function MainPage() {
   return (
     <>
-      <div className='ColumnSeparator'>
-        <div className='ButtonColumn'>
-          <div className='MainTitle'>Chess .</div>
-          <Button name='Play online' />
-          <Button name='Train yourself' secondary='true' />
-        </div>
+      <div>
+        <div className='MainTitle'>Chess .</div>
+        <Button name='Play online' />
+        <Button name='Train yourself' secondary='true' />
       </div>
     </>
   );
