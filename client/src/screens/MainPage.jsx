0945987a4922a6be98a040b3425656681613235e
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-04 17:06:37 -0400

    Changing the width of the border so that the buttons can be looking better when on hovering

--- client/src/screens/MainPage.jsx
+++ client/src/screens/MainPage.jsx
@@ -8,15 +8,11 @@ export default function MainPage() {
         style={{
           position: `absolute`,
           top: 0,
-          // bottom: 0,
           left: 0,
-          // right: 0,
           width: `100vw`,
           height: `100vh`,
           display: `flex`,
           flexDirection: `column`,
-          // alignItems: `center`,
-          // flexBasis: `auto`,
         }}
       >
         <div
@@ -24,7 +20,6 @@ export default function MainPage() {
             margin: `auto 50% auto 0`,
             display: 'flex',
             flexDirection: `column`,
-            // flexDirection: 'row-reverse',
           }}
         >
           <div className='MainTitle' style={{ alignSelf: 'flex-end' }}>
