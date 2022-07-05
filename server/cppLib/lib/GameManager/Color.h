2c2f8528790cc535969dc3f15b49539091bf1520
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-05 18:14:25 -0400

    Changing the server

--- server/cppLib/lib/GameManager/Color.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-enum class Color
-{
-    white = 0,
-    black = 1,
-    unknown = 2,
-};
--- server/cppLib/lib/GameManager/Position.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef struct Position
-{
-    int x;
-    int y;
-} Position;
