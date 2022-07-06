0edb4f4d47c69944156116fd7f22b0d86437f96a
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-07-05 21:10:23 -0400

    Working tests

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,11 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: Application = express();
 
-const add = (a: number, b: number): number => a + b;
+export const add = (a: number, b: number): number => a + b;
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello');
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello");
 });
 
-app.listen(5000, () => console.log('Server running'));
+app.listen(5001, () => console.log("Server running"));
