3aee866da1f9c1c919c4e02fa909df6f7e23b6ce
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-10 11:26:13 -0400

    refactoring the canvas component and adding select, unselect and move methods

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -5,6 +5,15 @@ function Canvas() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    setCanvasDimensions(canvas);
+
+    const context = canvas.getContext('2d');
+    contextRef.current = context;
+    drawCircle(context);
+  }, []);
+
   function setCanvasDimensions(canvas) {
     const SCREEN_ESTATE = 1.5; // change canvas size
     const ANTI_BLURRY_RATIO = 2; // change pixel density
@@ -35,16 +44,20 @@ function Canvas() {
     context.stroke();
   }
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    setCanvasDimensions(canvas);
+  function select() {}
 
-    const context = canvas.getContext('2d');
-    contextRef.current = context;
-    drawCircle(context);
-  }, []);
+  function unselect() {}
 
-  return <canvas ref={canvasRef}></canvas>;
+  function move() {}
+
+  return (
+    <canvas
+      onMouseDown={select}
+      onMouseUp={unselect}
+      onMouseMove={move}
+      ref={canvasRef}
+    ></canvas>
+  );
 }
 
 export default Canvas;
