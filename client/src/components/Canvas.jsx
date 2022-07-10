2f06cca1ec63b09e86cd97ff6afb3929a7b81405
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-10 10:21:23 -0400

    Adding a canvas functionnal component

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,24 +1,28 @@
-import React from "react";
-import "./Canvas.css"
+import React, { useRef, useEffect } from 'react';
+import './Canvas.css';
 
-class Canvas extends React.Component {
+function Canvas() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
 
-    draw(ctx) {
-        ctx.fillStyle = '#000000'
-        ctx.beginPath()
-        ctx.arc(50,100,20,0,2*Math.PI);
-        ctx.fill();
-    }
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
 
-    render(){
-        return (
-            <React.Fragment>
-                <canvas id="myCanvas" width="200" height="100">
-                </canvas>
-            </React.Fragment>
-        );
-    }
+    const context = canvas.getContext('2d');
+    context.scale(2, 2);
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+    context.lineWidth = 5;
+    contextRef.current = context;
 
+    
+  }, []);
+
+  return <canvas ref={canvasRef}></canvas>;
 }
 
 export default Canvas;
