9a5c3f59e4f3af296e7274e1e0275dcfe8ae572a
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-10 11:06:39 -0400

    setting canvas ratio for the chess board

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -5,21 +5,43 @@ function Canvas() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    canvas.width = window.innerWidth * 2;
-    canvas.height = window.innerHeight * 2;
-    canvas.style.width = `${window.innerWidth}px`;
-    canvas.style.height = `${window.innerHeight}px`;
+  function setCanvasDimensions(canvas) {
+    const SCREEN_ESTATE = 1.5; // change canvas size
+    const ANTI_BLURRY_RATIO = 2; // change pixel density
 
-    const context = canvas.getContext('2d');
+    const windowSize = {
+      height: window.innerHeight,
+      width: window.innerWidth,
+    };
+
+    if (windowSize.width > windowSize.height) {
+      canvas.width = windowSize.height * SCREEN_ESTATE;
+      canvas.height = windowSize.height * SCREEN_ESTATE;
+    } else {
+      canvas.width = windowSize.width * SCREEN_ESTATE;
+      canvas.height = windowSize.width * SCREEN_ESTATE;
+    }
+    canvas.style.width = `${canvas.width / ANTI_BLURRY_RATIO}px`;
+    canvas.style.height = `${canvas.height / ANTI_BLURRY_RATIO}px`;
+  }
+
+  function drawCircle(context) {
     context.scale(2, 2);
     context.lineCap = 'round';
     context.strokeStyle = 'black';
     context.lineWidth = 5;
-    contextRef.current = context;
+    context.beginPath();
+    context.arc(0, 0, 100, 0, 2 * Math.PI);
+    context.stroke();
+  }
 
-    
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    setCanvasDimensions(canvas);
+
+    const context = canvas.getContext('2d');
+    contextRef.current = context;
+    drawCircle(context);
   }, []);
 
   return <canvas ref={canvasRef}></canvas>;
