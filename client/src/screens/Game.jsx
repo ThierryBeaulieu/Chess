fc2f9af3f01c1caa74a0b5858ca5a031789aac31
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-10 11:09:47 -0400

    Centering the canvas in the board page

--- client/src/screens/Game.jsx
+++ client/src/screens/Game.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Canvas from '../components/Canvas';
 import './Game.css';
 
-
 export default function Game() {
-  return <Canvas/>;
+  return (
+    <div className='wrapper'>
+      <Canvas />
+    </div>
+  );
 }
