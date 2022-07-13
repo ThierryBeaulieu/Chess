45a01e68bc68c534e7a544b34a60882a6212b9ab
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-12 23:07:51 -0400

    Refactoring test

--- server/tests/template.test.ts
+++ server/tests/template.test.ts
@@ -2,26 +2,13 @@ import 'reflect-metadata';
 import { Container } from 'typedi';
 import GameService from '../src/services/game.service';
 
-describe('Update method', () => {
-  it('should update a specific existing Ingredient entry', () => {
-    function sum(a: number, b: number) {
-      return a + b;
-    }
-    expect(sum(1, 2)).toBe(3);
-  });
-});
-
-describe('Piece test', () => {
-  it('should update a specific existing Ingredient entry', () => {});
-});
-
 describe('Game server', () => {
   it('User shouldnt be able to fetch with bad player', async () => {
     const gameService = Container.get(GameService);
-    const fresult = await gameService.createGame(1, 2);
-    expect(fresult).toBe(true);
+    const firstResult = await gameService.createGame(1, 2);
+    expect(firstResult).toBe(true);
 
-    const sresult = await gameService.createGame(1, 3);
-    expect(sresult).toBe(false);
+    const secondResult = await gameService.createGame(1, 3);
+    expect(secondResult).toBe(false);
   });
 });
