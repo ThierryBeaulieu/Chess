344ba5d9a59c864a8fe9f6ef039900e8b2139b90
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-15 23:29:24 -0400

    adding a random string generator

---
+++ server/app/classes/Tools/Cookie.ts
@@ -0,0 +1,15 @@
+class Cookie {
+  private static CHARACTERS =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  public static generateSessionId(size: number): String {
+    let sessionId = '';
+
+    for (let i = 0; i < size; i++) {
+      sessionId += this.CHARACTERS.charAt(
+        Math.floor(Math.random() * this.CHARACTERS.length),
+      );
+    }
+    return sessionId;
+  }
+}
