f87af7dd388cc350e2c0512844d97bad10dca022
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-16 17:30:45 -0400

    setting cookie@

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -5,28 +5,35 @@ import MainPage from '../screens/MainPage';
 import Training from '../screens/Training';
 import Game from '../screens/Game';
 import GameManager from '../services/GameManager.service';
+import { CookiesProvider, useCookies } from 'react-cookie';
 
 function App() {
   const gameManager = new GameManager();
-  const [sessionId, setSessionId] = useState(async () => {
+  const [cookies, setCookie] = useCookies(['user']);
+  const [sessionId] = useState(async () => {
     return await gameManager.getSessionId();
   });
 
   useEffect(() => {
-    async function setSession() {
-      sessionStorage.setItem('sessionId', await sessionId);
+    async function setCookies() {
+      console.log(cookies);
+      if (cookies) {
+        setCookie('sessionId', await sessionId, { path: '/' });
+      }
     }
-    setSession();
-  }, []);
+    setCookies();
+  }, [sessionId, setCookie, cookies]);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<MainPage />}></Route>
-        <Route exact path='/game' element={<Game />}></Route>
-        <Route exact path='/training' element={<Training />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<MainPage />}></Route>
+          <Route exact path='/game' element={<Game />}></Route>
+          <Route exact path='/training' element={<Training />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
   );
 }
 
