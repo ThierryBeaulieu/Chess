c940d6ba43ae80ee0fc4e7ea3461d07b4e6ed987
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-15 21:32:35 -0400

    updating the data so that it can solve the conflict with predecent modifications

--- database/query.sql
+++ database/query.sql
@@ -1,11 +1,14 @@
 -- Exemple of queries to use in node.js
 -- DO NOT USE THIS FILE IN PRODUCTION
 
--- 1.user Asking for a user data when sessionId = '23asd'
+-- 1.user Asking for a user data
 SELECT * FROM chess.user WHERE sessionId='23asd';
 
-SELECT * FROM chess.player;
+-- 2.player Asking for a player's data
+SELECT * FROM chess.player WHERE playerId='42asd';
 
-SELECT * FROM chess.game;
+-- 3.game Asking for a game's data
+SELECT * FROM chess.game WHERE gameId='game1';
 
-SELECT * FROM chess.move;
+-- 4.move Asking for the moves made during a game
+SELECT * FROM chess.move WHERE gameId='game1';
