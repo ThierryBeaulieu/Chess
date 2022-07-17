194c590492712fe9ec36fe8d69d78cb421826d2a
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-16 22:00:44 -0400

    Adding a docker-compose configuration that allows to run postgres on docker

--- server/dist/app/classes/GameManager/Color.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Color;
-(function (Color) {
-    Color[Color["White"] = 0] = "White";
-    Color[Color["Black"] = 1] = "Black";
-    Color[Color["Unknown"] = 2] = "Unknown";
-})(Color || (Color = {}));
-exports.default = Color;
--- server/dist/app/classes/GameManager/GameManager.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class GameManager {
-    constructor() { }
-}
-exports.default = GameManager;
