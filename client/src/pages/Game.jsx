41ecd43318ddc7f81c97665a3989173ed8296517
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-18 22:20:56 -0400

    Adding Pieces

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Board from '../components/Board';
+import Pawn from '../components/Pieces/Pawn';
 import './Game.css';
 
 export default function Game() {
   return (
     <div className='wrapper'>
       <Board />
+      <Pawn />
     </div>
   );
 }
