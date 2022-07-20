61b760a6636f208e90028dce9aa57d7a52567126
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-19 21:44:28 -0400

    Adding a unified chess board

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Board from '../components/Board';
 import Pawn from '../components/Pieces/Pawn';
 import AlternateBoard from '../components/AlternateBoard';
+import UnifiedBoard from '../components/UnifiedBoard';
 import './Game.css';
 
 export default function Game() {
   return (
     <div className='wrapper'>
-      <AlternateBoard />
+      <UnifiedBoard />
     </div>
   );
 }
