8d1aa47a8669562eb33dff28fc29bfcfd8c051ea
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-19 21:38:53 -0400

    adding an alternative board made with photoshop instead of using a whole board

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Board from '../components/Board';
 import Pawn from '../components/Pieces/Pawn';
+import AlternateBoard from '../components/AlternateBoard';
 import './Game.css';
 
 export default function Game() {
   return (
     <div className='wrapper'>
-      <Board />
-      <Pawn />
+      <AlternateBoard />
     </div>
   );
 }
