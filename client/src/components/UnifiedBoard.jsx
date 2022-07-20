c8b0b82a29145cc2b581eea9bd22e7fd35c2b9ef
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-20 00:53:35 -0400

    Trying to making the board size adaptable to the board

--- client/src/components/UnifiedBoard.jsx
+++ client/src/components/UnifiedBoard.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
+import useWindowDimensions from './tools/WindowSizeHandler';
 import './AlternateBoard.css';
 
-export default class AlternateBoard extends React.Component {
-  windowSize;
+export default function AlternateBoard() {
+  const windowSize = useWindowDimensions();
 
-  constructor(props) {
-    super(props);
-    this.windowSize = props.windowDimensions;
+  function determineWindowSize() {
+    if (windowSize.width > windowSize.height) {
+      return windowSize.height * 0.8;
+    }
+    return windowSize.width * 0.8;
   }
 
-  render() {
-    console.log(this.windowSize.width);
-
-    return (
-      <svg
-        version='1.2'
-        xmlns='http://www.w3.org/2000/svg'
-        viewBox='0 0 800 800'
-        width={this.windowSize.width}
-        height={this.windowSize.height}
-      >
-        <title>board</title>
-        <defs>
-          <image
-            width={this.windowSize.width}
-            height={this.windowSize.height}
-            id='img1'
-            href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAyAAAAMgAQMAAADhvpQrAAAAAXNSR0IB2cksfwAAAAZQTFRFAFx739/fcqPYAAAAAVxJREFUeJzt2kENgEAQA8BzgH+XOIBvBdBHyfTX7CZjoOdEridSOkAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBLKKZLnz7cMDBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgf0Ay2+tBCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgEAgEAoFAukiW7fUgBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgXSSzvR6EQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgEAiki2TZXg9CIBAIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCATSRTLb60EIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUC6SJbt9SAEAoFAIBAIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCBV5AWz8azWX0fzrAAAAABJRU5ErkJggg=='
-          />
-        </defs>
-        <style></style>
-        <use id='Background' href='#img1' x='0' y='0' />
-      </svg>
-    );
-  }
+  return (
+    <svg
+      version='1.2'
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 800 800'
+      width={determineWindowSize()}
+      height={determineWindowSize()}
+    >
+      <title>board</title>
+      <defs>
+        <image
+          width={determineWindowSize()}
+          height={determineWindowSize()}
+          id='img1'
+          href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAyAAAAMgAQMAAADhvpQrAAAAAXNSR0IB2cksfwAAAAZQTFRFAFx739/fcqPYAAAAAVxJREFUeJzt2kENgEAQA8BzgH+XOIBvBdBHyfTX7CZjoOdEridSOkAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBLKKZLnz7cMDBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgf0Ay2+tBCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgEAgEAoFAukiW7fUgBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgXSSzvR6EQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUAgEAiki2TZXg9CIBAIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCATSRTLb60EIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCgUC6SJbt9SAEAoFAIBAIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCBV5AWz8azWX0fzrAAAAABJRU5ErkJggg=='
+        />
+      </defs>
+      <style></style>
+      <use id='Background' href='#img1' x='0' y='0' />
+    </svg>
+  );
 }
--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,45 +1,15 @@
 import React from 'react';
 import Board from '../components/Board';
-import Pawn from '../components/Pieces/Pawn';
+import Pawn from '../components/pieces/Pawn';
 import AlternateBoard from '../components/AlternateBoard';
 import UnifiedBoard from '../components/UnifiedBoard';
-import { useState, useEffect } from 'react';
 
 import './Game.css';
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
-
-function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions(),
-  );
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return windowDimensions;
-}
-
 export default function Game() {
-  const { width, height } = useWindowDimensions();
-  let windowSize = { width: width * 0.7, height: height * 0.7 };
-  // TODO: Use directly the width and the height property inside the unified board
-  // object.
   return (
     <div>
-      <UnifiedBoard windowDimensions={windowSize} />
+      <UnifiedBoard />
     </div>
   );
 }
