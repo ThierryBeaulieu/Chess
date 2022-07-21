4649b1b75361eb337222e5d1c9e7a4cef7b347a4
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-20 22:11:02 -0400

    adding piece

--- client/src/components/ChrisBoard.jsx
+++ client/src/components/ChrisBoard.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import Pawn from './pieces/Pawn';
 
 export default () => {
+  const cellSize = 100;
+  const columns = 4;
+
   return (
-    <div style={{ display: 'grid' }}>
-      {Array(64).map((_, index) => (
-        <div
-          key={index}
-          style={{
-            height: 10,
-            width: 10,
-            backgroundColor: index % 2 ? 'blue' : 'red',
-          }}
-        >
-          {index}
-        </div>
-      ))}
+    <div
+      style={{
+        display: 'grid',
+        width: 'fit-content',
+        gridTemplateColumns: `repeat(${columns}, auto)`,
+      }}
+    >
+      {Array.from(Array(columns).keys())
+        .map((_, i, arr) =>
+          arr.map((_, j) => (
+            <div
+              key={`${i}-${j}`}
+              style={{
+                height: cellSize,
+                width: cellSize,
+                backgroundColor: (i + j) % 2 ? 'blue' : 'red',
+              }}
+            >
+              <Pawn height={100} width={100} />
+            </div>
+          )),
+        )
+        .flat()}
     </div>
   );
 };
