18128d0ff8c56ded663df8c2736f276cfea593db
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-27 23:05:24 -0400

    Able to handle the data from the user in the form

--- client/src/components/PlayerForm.jsx
+++ client/src/components/PlayerForm.jsx
@@ -1,26 +1,53 @@
 import React from 'react';
 import './PlayerForm.css';
 import Button from '../components/Button';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function PlayerForm() {
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+
+  const handleFnameChange = (event) => {
+    setFname(event.target.value);
+  };
+
+  const handleLnameChange = (event) => {
+    setLname(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    console.log(`This was submited (${fname}) and (${lname})`);
+    alert(`This was submited (${fname}) and (${lname})`);
+    event.preventDefault();
+  };
+
   return (
     <>
-      <div className='bg-card'>
-        <div className='card-content'>
-          <h1>Player's info</h1>
-          <label>First name</label>
-          <input type='text' required />
-          <label>Last name</label>
-          <input type='text' required />
-        </div>
-
-        <Link to='/'>
-          <div className='startButton'>
-            <Button name='Start' />
+      <form onSubmit={handleSubmit}>
+        <div className='bg-card'>
+          <div className='card-content'>
+            <h1>Player's info</h1>
+            <label>First name</label>
+            <input
+              onChange={handleFnameChange}
+              type='text'
+              maxLength='150'
+              required
+            />
+            <label>Last name</label>
+            <input
+              onChange={handleLnameChange}
+              type='text'
+              maxLength='150'
+              required
+            />
           </div>
-        </Link>
-      </div>
+
+          <div className='startButton'>
+            <Button name='Start' type='submit' value='Submit' />
+          </div>
+        </div>
+      </form>
     </>
   );
 }
