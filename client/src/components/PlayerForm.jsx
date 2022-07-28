412eeec9e561e1a417a6aa74b92aff70573049f7
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-27 22:44:20 -0400

    Adding players name and last name field to the form

--- client/src/components/PlayerForm.jsx
+++ client/src/components/PlayerForm.jsx
@@ -7,7 +7,14 @@ export default function PlayerForm() {
   return (
     <>
       <div className='bg-card'>
-        <div className='card-content'>This is the content of the form</div>
+        <div className='card-content'>
+          <h1>Player's info</h1>
+          <label>First name</label>
+          <input type='text' required />
+          <label>Last name</label>
+          <input type='text' required />
+        </div>
+
         <Link to='/'>
           <div className='startButton'>
             <Button name='Start' />
