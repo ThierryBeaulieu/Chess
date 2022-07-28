ceb762d60d55e8749ef28118e56448707dc57625
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-27 22:31:51 -0400

    Adjusting the position of the start button to the right

--- client/src/components/PlayerForm.jsx
+++ client/src/components/PlayerForm.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './PlayerForm.css';
+import Button from '../components/Button';
+import { Link } from 'react-router-dom';
 
 export default function PlayerForm() {
   return (
     <>
       <div className='bg-card'>
         <div className='card-content'>This is the content of the form</div>
+        <Link to='/'>
+          <div className='startButton'>
+            <Button name='Start' />
+          </div>
+        </Link>
       </div>
     </>
   );
