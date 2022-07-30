3300b246dc296e92ac6fbc1b4841329527b96966
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2022-07-29 22:36:07 -0400

    Adding color variables to be more explicit

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -3,17 +3,21 @@ import { TestPiece } from './Pieces/TestPiece';
 import { useEffect, useState } from 'react';
 
 export default function Tile({ i, j, cellSize }) {
+  const YELLOW_TILE = '#e0de41';
+  const BLACK_TILE = '#d0b08a';
+  const WHITE_TILE = '#944c18';
+
   const [isShown, setIsShown] = useState(false);
   const [blackTile, setBlackTile] = useState('#d0b08a');
   const [whiteTile, setWhiteTile] = useState('#944c18');
 
   useEffect(() => {
     if (isShown) {
-      setBlackTile('#e0de41');
-      setWhiteTile('#e0de41');
+      setBlackTile(YELLOW_TILE);
+      setWhiteTile(YELLOW_TILE);
     } else {
-      setBlackTile('#d0b08a');
-      setWhiteTile('#944c18');
+      setBlackTile(BLACK_TILE);
+      setWhiteTile(WHITE_TILE);
     }
   }, [setBlackTile, setWhiteTile, isShown]);
 
